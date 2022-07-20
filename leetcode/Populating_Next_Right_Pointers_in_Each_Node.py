898d0bc6f472c2ab9b5777f925894cd611d12bad
long-doth2202 <long.doth2202@gmail.com>
2022-07-20 07:49:33 +0700

    algorithm day 8

---
+++ leetcode/Populating_Next_Right_Pointers_in_Each_Node.py
@@ -0,0 +1,28 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+class Solution:
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if root == None:
+            return root
+        
+        cur = root
+        dep_cur = cur.left
+
+        while cur.left:
+            cur.left.next = cur.right
+            if cur.next:
+                cur.right.next = cur.next.left
+                cur = cur.next
+            else:
+                cur = dep_cur
+                dep_cur = cur.left
+        
+        return root
