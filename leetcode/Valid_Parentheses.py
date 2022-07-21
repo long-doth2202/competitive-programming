cebaefbf8b3ba1ff5a81f84032f400b3cbb3cfc0
long-doth2202 <long.doth2202@gmail.com>
2022-07-21 07:23:52 +0700

    data structure day 9

--- leetcode/Valid_Parentheses.py
+++ leetcode/Valid_Parentheses.py
@@ -1,3 +1,17 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         map_brackets = {')': '(', ']': '[', '}': '{'}
+        stack = []
+
+        for c in s:
+            if c not in stack:
+                stack.append(c)
+            else:
+                if stack:
+                    last = stack.pop()
+                    if last != map_brackets[c]:
+                        return False
+                else:
+                    return False
+        
+        return False if stack else True
