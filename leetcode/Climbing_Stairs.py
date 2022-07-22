4e0d3f910307ed1193d094a4eb93549221e818ba
long-doth2202 <long.doth2202@gmail.com>
2022-07-22 08:09:13 +0700

    leetcode 75 day 10

--- leetcode/Climbing_Stairs.py
+++ leetcode/Climbing_Stairs.py
@@ -1,3 +1,10 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        F = [0 for i in range(n + 5)]
+
+        F[0], F[1] = 1, 1
+
+        for i in range(2, n + 1):
+            F[i] = F[i - 1] + F[i - 2]
         
+        return F[n]
