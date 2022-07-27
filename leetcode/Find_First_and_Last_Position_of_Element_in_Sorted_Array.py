7457c7415c0026931f751b5143b62a6ce446fc6d
long-doth2202 <long.doth2202@gmail.com>
2022-07-27 10:24:21 +0700

    algorithn day 15

---
+++ leetcode/Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -0,0 +1,21 @@
+from typing import List
+import bisect
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        ans = []
+        nums.append((1<<31))
+        
+        l = bisect.bisect_left(nums, target)
+        if l != len(nums) and nums[l] == target:
+          ans.append(l)
+        else:
+          ans.append(-1)
+
+        r = bisect.bisect_right(nums, target)
+        if (r - 1) >= 0 and nums[r - 1] == target:
+          ans.append(r)
+        else:
+          ans.append(-1)
+
+        return ans
---
+++ leetcode/Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+      left, right = 0, len(nums)
+      while left < right:
+        mid = (left + right) >> 1
+        if target < nums[0] < nums[mid]: # -inf
+          left = mid + 1
+        elif target >= nums[0] > nums[mid]: # inf
+          right = mid
+        elif nums[mid] < target:
+          left = mid + 1
+        elif nums[mid] > target:
+          right = mid
+        else:
+          return mid
+      
+      return -1
