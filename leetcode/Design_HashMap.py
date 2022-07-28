2b8155e1e42cd822f963c56bc4d4ad8b7bb930b6
long-doth2202 <long.doth2202@gmail.com>
2022-07-29 06:26:28 +0700

    data structure day 16

---
+++ leetcode/Design_HashMap.py
@@ -0,0 +1,13 @@
+class MyHashMap:
+
+    def __init__(self):
+        
+
+    def put(self, key: int, value: int) -> None:
+        
+
+    def get(self, key: int) -> int:
+        
+
+    def remove(self, key: int) -> None:
+        
